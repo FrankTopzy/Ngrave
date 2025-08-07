5f84fd67735fcd3fc051c2bac67fcbb72e92315d
Frank <temitopeadeoye787898@gmail.com>
2025-08-07 17:18:45 +0100

    section3 grid updated

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -12,14 +12,61 @@ import subBg6 from '../../assets/ngrave/ngrave-sub16.webp';
 import subBg7 from '../../assets/ngrave/ngrave-sub3.webp';
 import subBg8 from '../../assets/ngrave/ngrave-sub18.webp';
 import subBg9 from '../../assets/ngrave/ngrave-sub17.webp';
+import quoteImg1 from '../../assets/quotes/quote-1.png';
 import bunny from '../../assets/ngrave/bunny.svg';
 import metamask from '../../assets/ngrave/fox.svg';
 import { Link } from 'react-router-dom';
 import Navbar from '../../Components/Navbar/Navbar';
 import Sidebar from '../../Components/Sidebar/Sidebar';
+import { useRef } from 'react';
 
 // HOME PAGE / LANDING PAGE //
 function Home() {
+  //scroll by dragging
+  const gridContainer = useRef<HTMLDivElement>(null);
+
+  let isDown = false;
+  let startX: number;
+  let scrollLeft: number;
+
+  gridContainer?.current?.addEventListener('mousedown', (e) => {
+    isDown = true;
+
+    if (gridContainer.current) {
+      startX = e.pageX - gridContainer.current.offsetLeft;
+      scrollLeft = gridContainer.current.scrollLeft;
+      gridContainer.current.style.cursor = 'grabbing';
+    }
+  });
+
+  gridContainer?.current?.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+
+    if (gridContainer.current) {
+      const x: number = e.pageX - gridContainer.current.offsetLeft;
+      const walk = (x - startX) * 50;
+      gridContainer.current.scrollLeft = scrollLeft - walk;
+    }
+  });
+
+  gridContainer?.current?.addEventListener('mousemove', (e) => {
+    isDown = false;
+
+    if (gridContainer.current) {
+      gridContainer.current.style.cursor = 'grab';
+    }
+  });
+
+  gridContainer?.current?.addEventListener('mouseleave', () => {
+    isDown = false;
+
+    if (gridContainer.current) gridContainer.current.style.cursor = 'grab';
+  });
+
+
+
+
   return (
     <div className='relative'>
       <Navbar/>
@@ -178,7 +225,7 @@ function Home() {
           </p>
         </div>
         
-        <div className={`${Styles.discover_link} flex bg-[#0f1113] border-0 mb-[50px] rounded-2xl h-[700px] relative overflow-hidden`}>
+        <div className={`${Styles.discover_link} ${Styles.discover_link_others} flex bg-[#0f1113] border-0 mb-[50px] rounded-2xl h-[700px] relative overflow-hidden`}>
           <div className='pl-[111px] basis-[40%]'>
             <h1 className='text-5xl font-bold mb-5'>Tap and swipe. Just like a phone.</h1>
             <p>With a 4” ultra-clear touch screen, your crypto is at your fingertips. If you can use a phone, you can use NGRAVE ZERO.</p>
@@ -194,7 +241,7 @@ function Home() {
           </p>
         </div>
 
-        <div className={`${Styles.section3_grid} grid grid-cols-[repeat(auto-fill,minmax(450px,1fr))] gap-[1rem] text-[var(--text-color)]`}>
+        <div className={`${Styles.section3_grid} mb-[200px] grid grid-cols-[repeat(auto-fill,minmax(450px,1fr))] gap-[1rem] text-[var(--text-color)]`}>
 
           <div className={`${Styles.discover_link} relative bg-sky-700 border-none rounded-2xl overflow-hidden`}>
             <div className='border-none rounded-2xl p-10'>
@@ -206,7 +253,7 @@ function Home() {
 
             <p className={`${Styles.discover_link_link} absolute bottom-2 right-2 flex justify-between items-center text-[var(--link-color)] border-none rounded-[40px] overflow-hidden`}>
               <span className=' py-1 pl-3'>Video</span>
-              <span className="material-icons bg-black border-0 rounded-4xl py-2 px-2">add_2</span>
+              <span className="material-icons bg-black border-0 rounded-4xl py-2 px-2">add</span>
             </p>
           </div>
 
@@ -282,6 +329,139 @@ function Home() {
         </div>
       </section>
 
+      <div className='mb-[100px]'>
+        <div className='flex justify-between items-center px-[15%]'>
+          <h1 className='text-[52px] text-gray-600 flex flex-col leading-[60px]'>
+            Trusted <span className='text-white font-bold'>worldwide</span>
+          </h1>
+          <p className='flex flex-col text-[18px]'>
+            The most trusted hardware wallet on Trustpilot (4.7/5 stars).
+            <span className='text-[var(--link-color)] flex items-center gap-2'>Read all reviews on Trustpilot <span className="material-icons">arrow_forward_ios</span></span>
+          </p>
+        </div>
+
+        <div className='mx-[15%] justify-end items-center flex gap-3 mb-[40px]'>
+          <div className="material-icons bg-gray-600 p-2 rounded-4xl">
+            <span className="material-icons">arrow_back_ios</span>
+          </div>
+
+          <div className="material-icons bg-gray-600 border-0 rounded-4xl p-2">
+            <span className="material-icons">arrow_forward_ios</span>
+          </div>
+        </div>
+
+        <div>
+          <div className='overflow-x-scroll [scrollbar-width:none] [scroll-behavior:smooth]' ref={gridContainer}>
+            <div className='min-w-[3500px]'>
+              <div className='w-[400px] bg-[#0d0d16] p-8 border-none rounded-4xl inline-block ml-[270px] mr-7 cursor-grab select-none'>
+                <div className='flex items-center justify-center rounded-2xl border-[var(--link-color)] w-[50px] h-[50px] border-2 border-dashed mb-4'>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 17H9L11 13V7H5V13H8L6 17ZM14 17H17L19 13V7H13V13H16L14 17Z" fill="#3197FF"></path>
+                  </svg>
+                </div>
+
+                <div className="text-white lg:text-1xl text-xl font-bold leading-8 lg:leading-8"> “What sets NGRAVE apart is the seamless integration of these three products, never compromising on security, always achieving a frictionless experience.” </div>
+
+                <div className='justify-self-center mt-[100px]'>
+                  <img src={quoteImg1} alt="" className='justify-self-center'/>
+                  <p><span className="font-bold ">Jean Jacques Quisquater</span>, <br/>2nd reference Bitcoin Paper</p>
+                </div>
+              </div>
+
+              <div className='w-[400px] bg-[#0d0d16] p-8 border-none rounded-4xl inline-block mr-7 cursor-grab select-none'>
+                <div className='flex items-center justify-center rounded-2xl border-[var(--link-color)] w-[50px] h-[50px] border-2 border-dashed mb-4'>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 17H9L11 13V7H5V13H8L6 17ZM14 17H17L19 13V7H13V13H16L14 17Z" fill="#3197FF"></path>
+                  </svg>
+                </div>
+
+                <div className="text-white lg:text-1xl text-xl font-bold leading-8 lg:leading-8"> “What sets NGRAVE apart is the seamless integration of these three products, never compromising on security, always achieving a frictionless experience.” </div>
+
+                <div className='justify-self-center mt-[100px]'>
+                  <img src={quoteImg1} alt="" className='justify-self-center'/>
+                  <p><span className="font-bold ">Jean Jacques Quisquater</span>, <br/>2nd reference Bitcoin Paper</p>
+                </div>
+              </div>
+
+              <div className='w-[400px] bg-[#0d0d16] p-8 border-none rounded-4xl inline-block mr-7 cursor-grab select-none'>
+                <div className='flex items-center justify-center rounded-2xl border-[var(--link-color)] w-[50px] h-[50px] border-2 border-dashed mb-4'>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 17H9L11 13V7H5V13H8L6 17ZM14 17H17L19 13V7H13V13H16L14 17Z" fill="#3197FF"></path>
+                  </svg>
+                </div>
+
+                <div className="text-white lg:text-1xl text-xl font-bold leading-8 lg:leading-8"> “What sets NGRAVE apart is the seamless integration of these three products, never compromising on security, always achieving a frictionless experience.” </div>
+
+                <div className='justify-self-center mt-[100px]'>
+                  <img src={quoteImg1} alt="" className='justify-self-center'/>
+                  <p><span className="font-bold ">Jean Jacques Quisquater</span>, <br/>2nd reference Bitcoin Paper</p>
+                </div>
+              </div>
+
+              <div className='w-[400px] bg-[#0d0d16] p-8 border-none rounded-4xl inline-block mr-7 cursor-grab select-none'>
+                <div className='flex items-center justify-center rounded-2xl border-[var(--link-color)] w-[50px] h-[50px] border-2 border-dashed mb-4'>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 17H9L11 13V7H5V13H8L6 17ZM14 17H17L19 13V7H13V13H16L14 17Z" fill="#3197FF"></path>
+                  </svg>
+                </div>
+
+                <div className="text-white lg:text-1xl text-xl font-bold leading-8 lg:leading-8"> “What sets NGRAVE apart is the seamless integration of these three products, never compromising on security, always achieving a frictionless experience.” </div>
+
+                <div className='justify-self-center mt-[100px]'>
+                  <img src={quoteImg1} alt="" className='justify-self-center'/>
+                  <p><span className="font-bold ">Jean Jacques Quisquater</span>, <br/>2nd reference Bitcoin Paper</p>
+                </div>
+              </div>
+
+              <div className='w-[400px] bg-[#0d0d16] p-8 border-none rounded-4xl inline-block mr-7 cursor-grab select-none'>
+                <div className='flex items-center justify-center rounded-2xl border-[var(--link-color)] w-[50px] h-[50px] border-2 border-dashed mb-4'>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 17H9L11 13V7H5V13H8L6 17ZM14 17H17L19 13V7H13V13H16L14 17Z" fill="#3197FF"></path>
+                  </svg>
+                </div>
+
+                <div className="text-white lg:text-1xl text-xl font-bold leading-8 lg:leading-8"> “What sets NGRAVE apart is the seamless integration of these three products, never compromising on security, always achieving a frictionless experience.” </div>
+
+                <div className='justify-self-center mt-[100px]'>
+                  <img src={quoteImg1} alt="" className='justify-self-center'/>
+                  <p><span className="font-bold ">Jean Jacques Quisquater</span>, <br/>2nd reference Bitcoin Paper</p>
+                </div>
+              </div>
+
+              <div className='w-[400px] bg-[#0d0d16] p-8 border-none rounded-4xl inline-block mr-7 cursor-grab select-none'>
+                <div className='flex items-center justify-center rounded-2xl border-[var(--link-color)] w-[50px] h-[50px] border-2 border-dashed mb-4'>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 17H9L11 13V7H5V13H8L6 17ZM14 17H17L19 13V7H13V13H16L14 17Z" fill="#3197FF"></path>
+                  </svg>
+                </div>
+
+                <div className="text-white lg:text-1xl text-xl font-bold leading-8 lg:leading-8 select-none"> “What sets NGRAVE apart is the seamless integration of these three products, never compromising on security, always achieving a frictionless experience.” </div>
+
+                <div className='justify-self-center mt-[100px]'>
+                  <img src={quoteImg1} alt="" className='justify-self-center'/>
+                  <p><span className="font-bold ">Jean Jacques Quisquater</span>, <br/>2nd reference Bitcoin Paper</p>
+                </div>
+              </div>
+
+              <div className='w-[400px] bg-[#0d0d16] p-8 border-none rounded-4xl inline-block mr-7 cursor-grab select-none'>
+                <div className='flex items-center justify-center rounded-2xl border-[var(--link-color)] w-[50px] h-[50px] border-2 border-dashed mb-4'>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 17H9L11 13V7H5V13H8L6 17ZM14 17H17L19 13V7H13V13H16L14 17Z" fill="#3197FF"></path>
+                  </svg>
+                </div>
+
+                <div className="text-white lg:text-1xl text-xl font-bold leading-8 lg:leading-8"> “What sets NGRAVE apart is the seamless integration of these three products, never compromising on security, always achieving a frictionless experience.” </div>
+
+                <div className='justify-self-center mt-[100px]'>
+                  <img src={quoteImg1} alt="" className='justify-self-center'/>
+                  <p><span className="font-bold ">Jean Jacques Quisquater</span>, <br/>2nd reference Bitcoin Paper</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/*======================================== SECTION 4 =============================================*/}
       <section className={`${Styles.section4} bg-[var(--primary-color)] py-[60px] text-black`}>
         <p className='text-gray-500'>The Coldest Wallet</p>
@@ -347,6 +527,13 @@ function Home() {
           </table>
         </div>
       </section>
+
+      {/*======================================== SECTION 2 =============================================*/}
+      <section className={`${Styles.section5} ${Styles.slide_in_top} pt-[100px]`}>
+        
+ 
+      </section>
+      
 
       <a href='#section1' className='fixed right-[50px] bottom-[50px]  bg-[var(--link-color)]'>
         up
